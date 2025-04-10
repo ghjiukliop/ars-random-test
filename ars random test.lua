fd2667172748bcde08b30a127739f4358b9fde7d
ghjiukliop <152683933+ghjiukliop@users.noreply.github.com>
2025-04-10 23:19:57 +0700

    Update ars random test.lua

--- ars random test.lua
+++ ars random test.lua
@@ -2,7 +2,7 @@
 local player = game.Players.LocalPlayer
 
 -- Tìm kiếm SpikeMace6e39aa4 trong Inventory
-local spikeMace = player.leaderstats.Inventory.Weapons:FindFirstChild("SpikeMace6e39aa4")
+local spikeMace = player.leaderstats.Inventory.Weapons:FindFirstChild("SpikeMace05036dd")
 
 -- Kiểm tra xem vũ khí có tồn tại không
 if spikeMace then
